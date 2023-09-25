6411c13020845aa80bdb67bceaa40d23809e0d77
Brine <104254430+brinereef1@users.noreply.github.com>
2023-09-25 22:21:01 +0530

    Update index.js

--- express/index.js
+++ express/index.js
@@ -8,16 +8,22 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + "/public/index.html");
-// })
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
 
-app.post('/submit', (req, res) => {
-    console.log(req.body);
+app.post('/check', (req, res) => {
+    const password = req.body.password;
+
+    if (password === 'bitchass') {
+        res.send('Password matched lol!')
+    } else {
+        res.redirect('/')
+    }
 })
 
 app.listen(port, () => {
     console.log('server running')
-})
+});
